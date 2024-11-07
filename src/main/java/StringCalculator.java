edc5c0adb210e5d1a460f1d66da4b35de731b77b
Pranjal Gangwar <pranjal.gangwar@outlook.com>
2024-11-07 22:31:43 +0530

    adding case for empty string

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,5 +1,10 @@
 package main.java;
 
 public class StringCalculator {
-	
+	public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return -1;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,15 @@
 package test.java;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.StringCalculator;
+
 public class StringCalculatorTest {
+	@Test
+    public void testAdd_EmptyString_ReturnsZero() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""), "Empty string should return 0");
+    }
 }
