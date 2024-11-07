f49bc5943d28e945ff62ca4fe5386cb1521dc102
Pranjal Gangwar <pranjal.gangwar@outlook.com>
2024-11-07 22:33:24 +0530

    adding case for single number

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,6 +5,6 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        return -1;
+        return Integer.parseInt(numbers);
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -12,4 +12,10 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""), "Empty string should return 0");
     }
+	
+	@Test
+	public void testAdd_SingleNumber_ReturnsSameNumber() {
+	    StringCalculator calculator = new StringCalculator();
+	    assertEquals(1, calculator.add("1"), "Single number should return the number itself");
+	}
 }
